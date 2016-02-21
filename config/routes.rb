a0070082adc42d41921cfab8b8caee5c24cf98bc
Leo Burdianov <leo.burdianov@gmail.com>
2016-02-22 00:36:18 +0400

    enable users to place orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :listings
-  get 'pages/about'
+  
+  resources :listings do 
+    resources :orders
+  end
 
+  get 'pages/about'
   get 'pages/contact'
   root 'listings#index'
   get 'seller' => 'listings#seller'
